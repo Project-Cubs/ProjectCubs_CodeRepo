de70ec6954811f42d93d99e8cb85e7f0ab969f64
Brian Taesung Lee <blee421@gatech.edu>
2023-05-13 22:40:14 -0400

    update

--- src/utils/Firebase/Bookmark/bookmark.firebase.js
+++ src/utils/Firebase/Bookmark/bookmark.firebase.js
@@ -2,8 +2,7 @@ import { getUser, updateUser } from "../Users/users.firebase";
 
 export async function addBookmark(word) {
     // Get user's bookmark
-    const bookmark = await getBookmark();;
-
+    const bookmark = await getBookmark() || [];
     for (let i = 0; i < bookmark.length; i++) {
         if (bookmark[i].koreanWord === word.koreanWord) {
             alert("Already bookmarked");
@@ -14,7 +13,6 @@ export async function addBookmark(word) {
     await updateUser({
         bookmark: [...bookmark, word]
     })
-
 }
 
 export async function getBookmark() {
